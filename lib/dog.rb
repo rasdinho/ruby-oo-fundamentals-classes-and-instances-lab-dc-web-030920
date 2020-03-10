c895f2fac1052688066baa152719e2e5e74c14c4
Ras azzab <ras.dinho7@gmail.com>
2020-03-10 13:52:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,3 +4,16 @@
 # fido = dog.new 
 # snoopy = dog.new 
 # lassie = dog.new
+
+
+class Dog
+end
+ 
+fido = Dog.new
+# fido #=> #<Dog:0x007fc52c2d7d20>
+ 
+snoopy = Dog.new
+# snoopy #=> #<Dog:0x007fc52c2d4170>
+ 
+lassie = Dog.new
+# lassie #=> #<Dog:0x007fc52c2cc588>
